1e6b695d21fc49ea209ff2799b8597eb6753a586
Tim Garton <garton.tim@gmail.com>
2015-03-13 21:25:07 -0700

    fix ansible installation

--- install_ansible.sh
+++ install_ansible.sh
@@ -10,7 +10,8 @@ if [ ! -f "/usr/bin/ansible" ]; then
 
   #have to do this instead
   sudo add-apt-repository 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main'
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 
   sudo apt-get update
-  sudo apt-get -y -q install ansible
+  sudo apt-get -y install ansible
 fi
