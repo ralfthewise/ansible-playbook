20b20331dc8ccf1fb2d4611ae2765f8072470bc3
Tim Garton <garton.tim@gmail.com>
2016-05-05 21:04:57 -0700

    use pip to install ansible

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,15 +3,20 @@
 # This script installs ansible
 
 if [ ! -f "/usr/bin/ansible" ]; then
-  sudo apt-get -y -q install software-properties-common
+  sudo apt-get -y -q install software-properties-common build-essential
 
-  #would like to do this, but ansible hasn't release a PPA for utopic yet
+  #would like to do this, but ansible often takes months to update PPA for new OS releases
   #sudo add-apt-repository ppa:ansible/ansible
 
-  #have to do this instead
-  sudo add-apt-repository 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main'
-  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+  #instead you can do this, but even this doesn't work if ansible for a previous OS release
+  #won't work for the current release
+  #sudo add-apt-repository 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu wily main'
+  #sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 
-  sudo apt-get update
-  sudo apt-get -y install ansible
+  #sudo apt-get update
+  #sudo apt-get -y install ansible
+
+  #as a last resort, pip to the rescue!
+  sudo apt-get -y -q install python-pip python-setuptools libffi-dev libcurl4-openssl-dev libssl-dev
+  sudo pip install ansible
 fi
