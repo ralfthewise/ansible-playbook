2cea7d9af4c4cbf7dc1619444aa928c1b55af7b2
Tim Garton <garton.tim@gmail.com>
2017-11-12 14:09:02 -0800

    prompt for dirs to save in ~/dev

--- roles/reinstall_os/files/reinstall_prep.sh
+++ roles/reinstall_os/files/reinstall_prep.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -E
+set -o pipefail
+
 if [ $# -ne 2 ] ; then
   echo "Usage: $0 <backup location> <remote ssh backup location>"
   echo ""
@@ -10,62 +13,82 @@ BACKUP_DIR="$1"
 BACKUP_DIR="$(dirname "$BACKUP_DIR")/$(basename "$BACKUP_DIR")/"
 REMOTE_BACKUP="$2"
 
-mkdir -p "$BACKUP_DIR"
+run() {
+  mkdir -p "$BACKUP_DIR"
 
-ROOT="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${ROOT}"
-rsync -av $0 ~/bin/reinstall_finish.sh "$BACKUP_DIR"
+  ROOT="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  cd "${ROOT}"
+  rsync -av $0 ~/bin/reinstall_finish.sh "$BACKUP_DIR"
 
-#home folder
-printf "\n\e[32mProcessing home folder\e[0m\n"
-cd
-mkdir -p "$BACKUP_DIR/home"
-rsync -av --delete .ansible.cfg .bash_history .bash_logout .bashrc .gconf .gitconfig .irb-history .local .profile .pry_history .psql_history .ssh .VirtualBox .vnc bin tmp blog Documents Pictures Music Videos "$BACKUP_DIR/home/"
-rsync -av --delete --exclude .config/google-chrome .config "$BACKUP_DIR/home/"
-dconf dump / > "$BACKUP_DIR/home/current-dconf.dump"
+  #home folder
+  printf "\n\e[32mProcessing home folder\e[0m\n"
+  cd
+  mkdir -p "$BACKUP_DIR/home"
+  local BACKUP_ITEM
+  for BACKUP_ITEM in .ansible.cfg .bash_history .bash_logout .bashrc .gconf .gitconfig .irb-history .local .profile .pry_history .psql_history .ssh .VirtualBox .vnc bin tmp blog Documents Pictures Music Videos; do
+    [ -e "$BACKUP_ITEM" ] && rsync -av --delete "$BACKUP_ITEM" "$BACKUP_DIR/home/"
+  done
+  rsync -av --delete --exclude .config/google-chrome .config "$BACKUP_DIR/home/"
+  dconf dump / > "$BACKUP_DIR/home/current-dconf.dump"
 
-#/etc
-printf "\n\e[32mProcessing /etc\e[0m\n"
-sudo rsync -av --delete /etc/ "$BACKUP_DIR/etc"
+  #/etc
+  printf "\n\e[32mProcessing /etc\e[0m\n"
+  sudo rsync -av --delete /etc/ "$BACKUP_DIR/etc"
 
-#/root
-printf "\n\e[32mProcessing /root\e[0m\n"
-sudo mkdir -p "$BACKUP_DIR/root"
-sudo rsync -av /root/.bash_history "$BACKUP_DIR/root"
+  #/root
+  printf "\n\e[32mProcessing /root\e[0m\n"
+  sudo mkdir -p "$BACKUP_DIR/root"
+  sudo rsync -av /root/.bash_history "$BACKUP_DIR/root"
 
-#mdadm
-printf "\n\e[32mProcessing mdadm\e[0m\n"
-mkdir -p "$BACKUP_DIR/mdadm"
-sudo mdadm --detail --scan > "$BACKUP_DIR/mdadm/mdadm_detail_scan"
-sudo mdadm --detail /dev/md0 > "$BACKUP_DIR/mdadm/mdadm_detail_md0"
-sudo cat /proc/mdstat > "$BACKUP_DIR/mdadm/mdstat"
-cp /etc/fstab "$BACKUP_DIR/mdadm"
-sudo chown -R $USER "$BACKUP_DIR/mdadm"
-ssh $REMOTE_BACKUP "mkdir -p ~/tmp/reinstall/$BACKUP_DIR"
-rsync -av --delete "$BACKUP_DIR/mdadm" $REMOTE_BACKUP:"~/tmp/reinstall/$BACKUP_DIR"
+  #mdadm
+  printf "\n\e[32mProcessing mdadm\e[0m\n"
+  mkdir -p "$BACKUP_DIR/mdadm"
+  sudo mdadm --detail --scan > "$BACKUP_DIR/mdadm/mdadm_detail_scan"
+  sudo mdadm --detail /dev/md0 > "$BACKUP_DIR/mdadm/mdadm_detail_md0"
+  sudo cat /proc/mdstat > "$BACKUP_DIR/mdadm/mdstat"
+  cp /etc/fstab "$BACKUP_DIR/mdadm"
+  sudo chown -R $USER "$BACKUP_DIR/mdadm"
+  ssh $REMOTE_BACKUP "mkdir -p ~/tmp/reinstall/$BACKUP_DIR"
+  rsync -av --delete "$BACKUP_DIR/mdadm" $REMOTE_BACKUP:"~/tmp/reinstall/$BACKUP_DIR"
 
-##one offs
-printf "\n\e[32mProcessing one offs\e[0m\n"
+  ##one offs
+  printf "\n\e[32mProcessing one offs\e[0m\n"
 
-echo "Do you wish to sync everything in ~/dev/?"
-select yn in "Yes" "No"; do
-  case $yn in
-    Yes)
-      mkdir -p "$BACKUP_DIR/home/dev"
-      rsync -av ~/dev/ "$BACKUP_DIR/home/dev"
-      break
-      ;;
-    No)
-      break
-      ;;
-  esac
-done
+  mkdir -p "$BACKUP_DIR/home/dev"
+  dlg --visit-items --keep-tite --no-cancel --no-items --separate-output --checklist "What do you wish to sync in ~/dev?" 0 0 0 $(ls -p ~/dev | sed 's/$/ off/') | while read DEV_ITEM; do
+    rsync -av --delete --exclude '*node_modules/' --exclude '*vendor/' ~/dev/"$DEV_ITEM" "$BACKUP_DIR/home/dev/$DEV_ITEM"
+  done
 
-#mkdir -p "$BACKUP_DIR/home/dev/idexperts"
-#cd ~/dev/idexperts
-#rsync -av ansible.passwd ops-windows-dev radartools-aws-dev-2015 radartools-aws-dev.pem rdesktop.sh scp.sh ssh.sh "$BACKUP_DIR/home/dev/idexperts"
-#cd -
+  rsync -av ~/VirtualBox\ VMs "$BACKUP_DIR/home/"
+}
 
-rsync -av ~/VirtualBox\ VMs "$BACKUP_DIR/home/"
+dlg() {
+  trap - ERR # dialog uses return code to communicate cancellation
+  dialog "$@" 2>&1 >&101
+  local STATUS=$?
+  add_err_handler # re-add err handler
 
-#printf "\n\e[31mDon't forget to examine ~/dev for things that need to be saved\e[0m\n"
+  if [ $STATUS -eq 1 ] || [ $STATUS -eq 255 ]; then
+    echo "Dialog cancelled" >&101
+    exit $STATUS
+  fi
+  return $STATUS
+}
+
+#the usual housekeeping
+exec 101>&1 # point 101 at raw stdout
+# halt on any error
+HALT_STATUS=77 # magic exit code to cascade termination up through parent processes
+handle_err() {
+  local RETVAL=$?
+  local LINE=$1
+  [ $RETVAL -ne $HALT_STATUS ] || exit $HALT_STATUS
+  echo "Error occurred at line $LINE: $BASH_COMMAND"
+  exit $HALT_STATUS
+}
+add_err_handler() {
+  trap 'handle_err $LINENO' ERR
+}
+add_err_handler
+
+run
