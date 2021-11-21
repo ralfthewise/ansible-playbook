3448d3cc65f89d40eba94d0ab559d713a6456174
Tim Garton <garton.tim@gmail.com>
2021-11-21 06:31:23 +0000

    fixes for 20.04

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,8 +4,9 @@
 ROOT="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "${ROOT}" &> /dev/null
 
-#as a last resort, pip to the rescue!
-sudo apt-get -y -q install software-properties-common build-essential python-pip python-setuptools libffi-dev libcurl4-openssl-dev libssl-dev
-sudo -i pip install --upgrade pip
-[ -e venv ] || virtualenv venv
-source venv/bin/activate && pip install -r requirements.txt
+sudo apt -y install software-properties-common build-essential libffi-dev libcurl4-openssl-dev libssl-dev python3-dev
+sudo apt -y install python3.8-venv || sudo apt install -y python3.6-venv
+[ -e .venv ] || python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip wheel
+pip install -r requirements.txt
--- provision.sh
+++ provision.sh
@@ -5,7 +5,7 @@ pushd "${ROOT}" &> /dev/null
 
 #first make sure ansible is installed
 ./install_ansible.sh
-source venv/bin/activate
+source .venv/bin/activate
 
 #configure what will be installed via ansible
 ANSIBLE_HOST="localhost"
@@ -13,7 +13,7 @@ ANSIBLE_GROUPS="localhost,vim"
 export ANSIBLE_HOST ANSIBLE_GROUPS
 
 #fucking do this shit, bitch!!
-ansible-playbook site.yml -vv -i dynamic.py --ask-become-pass "$@"
+ansible-playbook site.yml -vv -i hosts.yml --ask-become-pass "$@"
 
 echo "The following startup applications will be launched upon next login: caffeine-indicator, xbindkeys"
 popd &> /dev/null
