57afbdccff942acca7fc1a6198c4cc6bb30edcc7
Tim Garton <garton.tim@gmail.com>
2026-09-05 12:33:57 -0700

    some updates for ubuntu 26.04

--- install_ansible.sh
+++ install_ansible.sh
@@ -5,7 +5,7 @@ ROOT="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "${ROOT}" &> /dev/null
 
 sudo apt -y install software-properties-common build-essential libffi-dev libcurl4-openssl-dev libssl-dev python3-dev
-sudo apt -y install python3.12-venv || sudo apt -y install python3.8-venv || sudo apt install -y python3.6-venv
+sudo apt -y install python3.14-venv || sudo apt -y install python3.12-venv || sudo apt -y install python3.8-venv || sudo apt install -y python3.6-venv
 [ -e .venv ] || python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
